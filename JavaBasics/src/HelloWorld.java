f5bf7b8f7c3ca0acf0d8f76e198b073a7eba70e8
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2022-04-22 16:10:14 +0300

    Static blocks

--- JavaBasics/src/HelloWorld.java
+++ JavaBasics/src/HelloWorld.java
@@ -2,4 +2,12 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    static {
+        System.out.println("Static Block 1");
+    }
+
+    static {
+        System.out.println("Static Block 2");
+    }
 }
