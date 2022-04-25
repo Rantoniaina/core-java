35893afb9e3c1872506f7305d2c4fa864e0d233f
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2022-04-25 08:50:39 +0300

    Create a static reference directly

--- NonStaticMembers/src/Demo.java
+++ NonStaticMembers/src/Demo.java
@@ -1,11 +1,13 @@
 public class Demo {
 
+    static  Demo obj;
+
     static {
-        Demo obj = new Demo();
         System.out.println(obj);
+        Demo.obj = new Demo();
     }
 
     public static void main(String[] args) {
-
+        System.out.println(Demo.obj);
     }
 }
