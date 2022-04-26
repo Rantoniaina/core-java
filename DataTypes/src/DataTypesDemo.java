81f40c26d44833d9e0ceb3ada927c1455ea03edb
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2022-04-26 08:34:12 +0300

    Data type in action

---
+++ DataTypes/src/DataTypesDemo.java
@@ -0,0 +1,18 @@
+public class DataTypesDemo {
+
+    public static void main(String[] args) {
+
+        byte a = -50;
+        short b = 150;
+        int c = 10000;
+        long d = 100000;
+        char e = 'A';
+
+        float f = 1.23f;
+        double g = 121345.6789;
+
+        byte h = 20;
+
+        int result = a + h;
+    }
+}
