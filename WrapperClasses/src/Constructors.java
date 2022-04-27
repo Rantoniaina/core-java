5d922bcb4183638de5b6e96192253e7e149035b3
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2022-04-27 08:27:49 +0300

    Wrapper type constructors

---
+++ WrapperClasses/src/Constructors.java
@@ -0,0 +1,11 @@
+public class Constructors {
+    public static void main(String[] args) {
+        long l = 1000;
+        // Deprecated use valueOf
+        Long a = new Long(l);
+
+        String s = "2000";
+        // Deprecated use valueOf
+        Long b = new Long(s);
+    }
+}
