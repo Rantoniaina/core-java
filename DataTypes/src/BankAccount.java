699ed00e7916521264e54c84a91e1d88bd2478db
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2022-04-27 08:13:47 +0300

    More programs: Data types

---
+++ DataTypes/src/BankAccount.java
@@ -0,0 +1,12 @@
+public class BankAccount {
+
+    public static void main(String[] args) {
+        int accNo = 12345;
+        String name = "John";
+        double balance = 1000000.123;
+        String type = "Savings";
+        boolean active = false;
+
+        System.out.println(active);
+    }
+}
