e7cf2d6e371b8ee78812e43349bdc192de809a44
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2022-04-27 08:24:50 +0300

    String and objects

---
+++ WrapperClasses/src/ObjectAndString.java
@@ -0,0 +1,8 @@
+public class ObjectAndString {
+    public static void main(String[] args) {
+        long x = 1000;
+        Long y = Long.valueOf(x);
+        String s = y.toString();
+        Long w = Long.valueOf(s);
+    }
+}
