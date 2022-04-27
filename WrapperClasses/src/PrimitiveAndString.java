d8f4f9edad28a19bacbefa4520270294b7618bca
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2022-04-27 08:21:48 +0300

    Primitives and string

---
+++ WrapperClasses/src/PrimitiveAndString.java
@@ -0,0 +1,7 @@
+public class PrimitiveAndString {
+    public static void main(String[] args) {
+        byte x = 100;
+        String s = Byte.toString(x);
+        byte z = Byte.parseByte(s);
+    }
+}
