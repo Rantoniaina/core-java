cf248a550ce3f09700d84c82ca3b90c8567b199e
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2022-04-28 08:24:47 +0300

    Arithmetic operators

---
+++ Operators/src/ArithmeticOperators.java
@@ -0,0 +1,10 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int x = 20, y = 3;
+        System.out.println("Addition: " + (x + y));
+        System.out.println("Substraction: " + (x - y));
+        System.out.println("Multilication: " + (x * y));
+        System.out.println("Division: " + (x / y));
+        System.out.println("Reminder: " + (x % y));
+    }
+}
