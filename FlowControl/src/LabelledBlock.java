f2edb733ba54bfb678f9a8061b821b901b17c8e6
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2022-04-29 09:04:41 +0300

    Labelled blocks and break

---
+++ FlowControl/src/LabelledBlock.java
@@ -0,0 +1,16 @@
+public class LabelledBlock {
+    public static void main(String[] args) {
+        int x = 20;
+
+        l1:
+        {
+            System.out.println("Block begins");
+            if (x == 20) {
+                break l1;
+            }
+            System.out.println("Block ends");
+        }
+
+        System.out.println("Outside the block");
+    }
+}
