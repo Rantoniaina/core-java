bff51cc3388ab159bcf4fdf0ef75bab9b5b0bab7
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2022-10-06 17:42:59 +0300

    More about switches

---
+++ FlowControl/src/SwitchCase.java
@@ -0,0 +1,20 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        int a = 10;
+        final int b = 20;
+
+        switch (a) {
+            case 10:
+                System.out.println("Case 10");
+                break;
+            case b + 1:
+                System.out.println("Case 20");
+                break;
+            case 30:
+                System.out.println("Case 30");
+                break;
+            default:
+                System.out.println("No Matches");
+        }
+    }
+}
