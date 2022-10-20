66864d44223818f65f866cd4d7639b2d52fc4e49
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2022-10-20 17:00:27 +0300

    Sub packages

--- Packages/src/p1/B.java
+++ Packages/src/p1/B.java
@@ -1,9 +1,14 @@
 package p1;
 
+import p1.p3.E;
+
 public class B {
     public static void main(String[] args) {
         A.a1();
         A aObject = new A();
         aObject.a2();
+
+        E eObject = new E();
+        eObject.e1();
     }
 }
--- Packages/src/p2/C.java
+++ Packages/src/p2/C.java
@@ -1,6 +1,7 @@
 package p2;
 
 import p1.A;
+import p1.p3.E;
 
 public class C {
     public static void main(String[] args) {
@@ -9,5 +10,8 @@ public class C {
         aObject.a2();
 
         p1.B bObject = new p1.B();
+
+        E eObject = new E();
+        eObject.e1();
     }
 }
