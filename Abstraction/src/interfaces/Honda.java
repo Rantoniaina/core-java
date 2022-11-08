2258ac442eded0704a0b8deb3cf828cea3161cfa
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2022-11-08 17:52:41 +0300

    Create an interface

---
+++ Abstraction/src/interfaces/Honda.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+public class Honda implements Car {
+    @Override
+    public void go() {
+        System.out.println("Inside Honda go");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Inside Honda stop");
+    }
+}
